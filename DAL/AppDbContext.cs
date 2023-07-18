daf871d0eed926ec4fa581811c9d29ea554eeaf7
Ilya Petrov <ipb.tech@yandex.ru>
2023-07-18 16:09:11 +0300

    Add files via upload

---
+++ DAL/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyFirstWebApp.DAL.Configuration;
+using MyFirstWebApp.Domain;
+
+namespace MyFirstWebApp.DAL
+{
+    public class AppDbContext : DbContext
+    {  
+        public DbSet<Book> Books { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().ToTable("BooksVision");
+            modelBuilder.ApplyConfiguration(new BookDbConfig());
+        }
+    }
+}
---
+++ Services/BookService.cs
@@ -0,0 +1,47 @@
+﻿using MyFirstWebApp.DAL;
+using MyFirstWebApp.Domain;
+
+namespace MyFirstWebApp.Services
+{
+    public class BookService
+    {
+        private readonly AppDbContext _dbContext;
+
+        public BookService(AppDbContext appDbContext)
+        {
+            _dbContext = appDbContext;
+        }
+        public List<Book> GetAll()
+        {
+            return _dbContext.Books.ToList();
+        }
+        public Book GetById(long id)
+        {
+            return _dbContext.Books.FirstOrDefault(x => x.Id == id);
+        }
+        public async void CreateBookAsync(Book book)
+        {
+            await _dbContext.Books.AddAsync(book);
+        }
+        public void RemoveByIdAsync(long id)
+        {
+            var removedBook = GetById(id);
+            try
+            {
+                _dbContext.Remove(removedBook);
+            }
+            catch (ArgumentNullException)
+            {
+                throw new ArgumentNullException("Книга не найдена!");
+            }
+        }
+        public void UpdateEntity(Book book)
+        {
+            _dbContext.Books.Update(book);
+        }
+        public async Task SaveChangesAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
