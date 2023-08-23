f6d654c0016d06c573de3d1e9ab5ade70ed0fa65
Ilya Petrov <ipb.tech@yandex.ru>
2023-08-23 23:13:20 +0300

    Start to rewrite om MVC

---
+++ BookVisionWebApp/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace BookVisionWebApp.Models
+{
+    public class Book
+    {  
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public double Price { get; set; }
+        public string? Description { get; set; }
+
+        public Book(string title, string author, double price)
+        {
+            Title = title;
+            Author = author;
+            Price = price;
+        }
+    }
+}
